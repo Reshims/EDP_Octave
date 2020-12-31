ec81c7d2c434ab949e7099917c52b93456d44e4e
Reshims <thibault.fiasse@gmail.com>
2020-12-31 14:17:53 +0100

    Update main.m

--- main.m
+++ main.m
@@ -11,9 +11,9 @@ lnm = length(mfilename());
 FLP = FLP(1:end-lnm);
 
 PATHS = {
-  [FLP 'onde\onde_rect.m'],                      %wave -> rect -> u(t=0) = f, du/dt(t=0) = g
-  [FLP 'laplace\laplace_circ.m'],                %lapl -> circ -> u(r=a) = f
-  [FLP 'laplace\laplace_rect_3hom.m']            %lapl -> rect -> u(x=L) = f
+  [FLP 'onde/onde_rect.m'],                      %wave -> rect -> u(t=0) = f, du/dt(t=0) = g
+  [FLP 'laplace/laplace_circ.m'],                %lapl -> circ -> u(r=a) = f
+  [FLP 'laplace/laplace_rect_3hom.m']            %lapl -> rect -> u(x=L) = f
 };
 
 if !have_window_system(),
@@ -21,10 +21,10 @@ if !have_window_system(),
   return
 end
 
-source([FLP 'utils\callbacks.m'])
-source([FLP 'utils\math_funs.m'])
-source([FLP 'utils\utils.m'])
-source([FLP 'utils\window_utils.m'])
+source([FLP 'utils/callbacks.m'])
+source([FLP 'utils/math_funs.m'])
+source([FLP 'utils/utils.m'])
+source([FLP 'utils/window_utils.m'])
 
 global DLG
 appli_gui(pname)
